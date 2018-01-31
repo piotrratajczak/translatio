f1ddf56c7abb31a85bf9964bad6d3d07caa4d2a5
ratajczak <piotr.ratajczak@recoded.co>
2018-01-31 16:16:42 +0100

    translation compoent, editing translations - in progress

--- translation-manager/src/components/LangPage.js
+++ translation-manager/src/components/LangPage.js
@@ -8,33 +8,62 @@ class LangPage extends Component {
 
 		this.state = {
 			lang: null,
-			langData: {}
+			data: {}
 		};
 
 		this.checkLang = this.checkLang.bind(this);
+		this.handleTranslationChange = this.handleTranslationChange.bind(this);
+		this.handleSingleSave = this.handleSingleSave.bind(this);
 	}
 
-	componentWillMount() {}
+	componentWillMount() {
+		this.checkLang();
+	}
 
-	componentWillReceiveProps(nextProps) {}
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.lang !== this.props.lang) {
+			this.checkLang(nextProps);
+		}
+	}
 
-	checkLang(props = this.props) {
-		if (props.lang !== this.state.lang) {
-			console.log('LANG CHANGE TODO!');
+	handleSingleSave(tag) {
+		console.log(tag);
+	}
+
+	handleTranslationChange({ target }) {
+		let tag = target.id;
+		let value = target.value;
+
+		this.setState({
+			data: Object.assign({}, this.state.data, { [tag]: value })
+		});
+	}
+
+	checkLang({ lang, data } = this.props) {
+		if (lang !== this.state.lang) {
+			this.setState({ lang, data });
 		}
 	}
 
 	// also todo saving form function
 
 	render() {
-		const { data } = this.props;
+		const { data } = this.state;
+		let originalData = this.props.data;
 		return (
 			<Form>
 				{data &&
 					Object.keys(data)
 						.sort()
 						.map(tag => (
-							<Translation tag={tag} translation={data[tag]} key={tag} />
+							<Translation
+								tag={tag}
+								translation={data[tag]}
+								key={tag}
+								original={originalData[tag]}
+								onChange={this.handleTranslationChange}
+								onSave={this.handleSingleSave}
+							/>
 						))}
 			</Form>
 		);
--- translation-manager/src/components/Translation.js
+++ translation-manager/src/components/Translation.js
@@ -1,18 +1,43 @@
+import './Translation.css';
 import React from 'react';
-import { FormGroup, Label, Input, FormText } from 'reactstrap';
+import {
+	InputGroupAddon,
+	InputGroup,
+	Button,
+	FormGroup,
+	Label,
+	Input,
+	FormText,
+	Badge
+} from 'reactstrap';
 
 //todo maybe form text if text is longer than ...xxxx
 
-const Translation = ({ tag, translation }) => (
-	<FormGroup>
-		<Label for={tag}>{tag}:</Label>
-		<Input
-			type="text"
-			id={tag}
-			value={translation}
-			placeholder="NO TRANSLATION"
-		/>
-	</FormGroup>
-);
+const Translation = ({ tag, translation, onChange, original, onSave }) => {
+	const changed = translation !== original;
+	return (
+		<FormGroup>
+			<Label for={tag}>
+				{tag}:{changed && <Badge color="danger">Unsaved Change!</Badge>}
+			</Label>
+			<InputGroup>
+				<Input
+					onChange={onChange}
+					type="text"
+					id={tag}
+					value={translation}
+					placeholder="NO TRANSLATION"
+				/>
+				{changed && (
+					<InputGroupAddon>
+						<Button onClick={evt => onSave(tag)} color="primary">
+							Save only this change
+						</Button>
+					</InputGroupAddon>
+				)}
+			</InputGroup>
+		</FormGroup>
+	);
+};
 
 export default Translation;
