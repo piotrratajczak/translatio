4a30da00397f405ba68edbe395f7bd4bd15569db
ratajczak <piotr.ratajczak@recoded.co>
2018-02-05 17:33:48 +0100

    styling

--- translation-manager/src/components/StartPage.js
+++ translation-manager/src/components/StartPage.js
@@ -1,5 +1,6 @@
 import './StartPage.css';
 import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import React from 'react';
 import startImg from '../img/startPage.jpg';
@@ -7,15 +8,22 @@ import startImg from '../img/startPage.jpg';
 const StartPage = ({ languages }) => (
 	<div>
 		<img src={startImg} className="img-fluid" alt="start page translations" />
-		<h4>Avaible translation file downloads:</h4>
+		<h4>Available languages:</h4>
 		<ListGroup className="horizontal-list">
 			{languages.map(langCode => (
 				<ListGroupItem key={langCode}>
+					<h6>{langCode}</h6>
+					<Link className="btn btn-primary btn-sm" to={`/lang/${langCode}`}>
+						Edit
+					</Link>
 					<a
-						className="btn btn-primary"
+						className="btn btn-info btn-sm"
 						href={`/api/lang/file/${langCode}`}
-						download={`${langCode}.json`}>
-						{langCode}
+						download={`${langCode}.json`}
+						onClick={e => {
+							e.stopPropagation();
+						}}>
+						Download
 					</a>
 				</ListGroupItem>
 			))}
