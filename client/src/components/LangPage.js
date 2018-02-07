87ed663a8ca9c9bc3b713d47396efef193de8f3b
ratajczak <piotr.ratajczak@recoded.co>
2018-02-07 08:17:59 +0100

    show info at langPage if no tags created yet

--- client/src/components/LangPage.js
+++ client/src/components/LangPage.js
@@ -1,10 +1,20 @@
+import './LangPage.css';
 import { Form } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import React from 'react';
 import Translation from './Translation';
 
 const LangPage = ({ data, onSave }) => (
-	<Form>
+	<Form className="lang-page">
+		{!Object.keys(data).length && (
+			<div className="info-text">
+				<p>There are no tags yet! Please add one!</p>
+				<Link className="btn btn-warning btn-sm" to="/add/tag">
+					Add tag
+				</Link>
+			</div>
+		)}
 		{data &&
 			Object.keys(data)
 				.sort()
