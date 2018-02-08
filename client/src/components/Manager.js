e18ec4223cb4c98837e496d86ec5bff2d52644a4
ratajczak <piotr.ratajczak@recoded.co>
2018-02-08 15:05:31 +0100

    prop name fix lang -> langCode

--- client/src/components/Manager.js
+++ client/src/components/Manager.js
@@ -140,7 +140,7 @@ class Manager extends Component {
 					data[langCode] && (
 						<LangPage
 							data={langData}
-							lang={langCode}
+							langCode={langCode}
 							onSave={this.handleSave}
 							onDelete={this.handleTagDelete}
 							onAddClick={this.showModal}
